79b0a78afc2446e6e733d65820d0e9b8bb35fdb4
Mulubi <mulubiwarren@gmail.com>
2022-08-05 08:24:49 -0700

    Prototypes

---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
+
+#define BUFSIZE 1024
+#define TOK_BUFSIZE 128
+#define TOK_DELIM " \t\r\n\a"
+
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+
+#endif
